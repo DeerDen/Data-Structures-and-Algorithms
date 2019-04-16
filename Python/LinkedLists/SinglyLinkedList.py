38fcef240f9c5d6f105a9f41f4ae4ac611e27022
Furqan <36116798+Furqan17@users.noreply.github.com>
2019-04-16 09:36:55 -0400

    add Python - LinkedList RunScript

--- Python/LinkedLists/SinglyLinkedList.py
+++ Python/LinkedLists/SinglyLinkedList.py
@@ -17,3 +17,25 @@ class Node(object):
             node = node.next
         return 
     
+if __name__ == "__main__":
+    
+    # init linkedlist of 6 nodes in ascending order
+    node1 = Node(3)
+    node2 = Node(14)
+    node3 = Node(25)
+    node4 = Node(32)
+    node5 = Node(50)
+    node6 = Node(61)
+    
+    # connect nodes 
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+    node5.next = node6
+
+    # the linkedlist looks like
+    # 3 -> 14 -> 15 -> 32 -> 50 -> 61
+    
+    # run this file to initialize the linkedlist
+    # call node1.traverse() to see the full linked list traversal

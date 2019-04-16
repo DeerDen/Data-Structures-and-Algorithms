1c540338bc34ee929f29d2f446fe02dccb4ba619
Furqan <36116798+Furqan17@users.noreply.github.com>
2019-04-16 09:30:24 -0400

    add Python - LinkedList NodeClass

--- Python/LinkedLists/SinglyLinkedList.py
+++ Python/LinkedLists/SinglyLinkedList.py
@@ -1,2 +1,19 @@
-print(1)
+class Node(object):
+    
+    def __init__(self, val): # note this is a singly linked list
+        self.val = val
+        self.next = None # point to nothing initially
+
+    def get_val(self): # return current value 
+        return self.val
+
+    def get_next(self): # return next value
+        return self.next.val
+
+    def traverse(self): # traverse through the full list
+        node = self
+        while node != None: # print val and increment node
+            print(node.val)
+            node = node.next
+        return 
     

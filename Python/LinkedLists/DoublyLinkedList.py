2ce09a885adf41083d82f2c57c493608ca724116
Furqan <36116798+Furqan17@users.noreply.github.com>
2019-04-17 12:57:18 -0400

    add DoublyLinkedList NodeClass

--- Python/LinkedLists/DoublyLinkedList.py
+++ Python/LinkedLists/DoublyLinkedList.py
@@ -1,1 +1,33 @@
+class Node(object):
+    
+    def __init__(self, val): # doubly linked list
+        self.val = val
+        self.next = None # points to nothing initially
+        self.prev = None
 
+    def get_val(self): # return current value 
+        return self.val
+
+    def get_next(self): # return next value
+        return self.next.val
+
+    def get_prev(self): # return previous value
+        return self.prev
+
+    def traverse(self): # traverse through the full list
+        node = self
+        while node != None:
+            print(node.val)
+            node = node.next
+        return
+
+    # node1.traverse() - to traverse forward
+
+    def traverse_back(self): # traverse backwards through the list
+        node = self
+        while node != None:
+            print(node.val)
+            node = node.prev
+        return
+
+    # node6.traverse_back() - to traverse backwards

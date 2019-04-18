0cdf76410d4df2136506c7f675c5bff66d7364b9
Furqan <36116798+Furqan17@users.noreply.github.com>
2019-04-18 15:35:47 -0400

    DoublyLinkedList RunScript

--- Python/LinkedLists/DoublyLinkedList.py
+++ Python/LinkedLists/DoublyLinkedList.py
@@ -31,3 +31,36 @@ class Node(object):
         return
 
     # node6.traverse_back() - to traverse backwards
+
+    
+if __name__ == "__main__":
+  
+    # init linkedlist of 6 nodes in ascending order
+    node1 = Node(2)
+    node2 = Node(12)
+    node3 = Node(23)
+    node4 = Node(36)
+    node5 = Node(47)
+    node6 = Node(51)
+    
+    # Connect nodes 
+    node1.next = node2 # node1 - head node
+
+    node2.prev = node1
+    node2.next = node3
+
+    node3.prev = node2
+    node3.next = node4
+
+    node4.prev = node3
+    node4.next = node5
+
+    node5.prev = node4
+    node5.next = node6
+
+    node6.prev = node5
+
+    # Final linkedlist looks like
+    # 2 (head) <=> 12 <=> 23 <=> 36 <=> 47 <=> 51
+    
+    # Run this file to create the linkedlist
